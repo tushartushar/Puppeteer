ae79ff32a3efbf4169a37a9aa65a6c8e24824416
Marios Fragkoulis <mfg@aueb.gr>
2016-01-15 14:08:55 +0200

    Removed obsolete DEFINE_REGEX.

--- SourceModel/SM_Constants.py
+++ SourceModel/SM_Constants.py
@@ -1,6 +1,5 @@
 CLASS_REGEX = r'class \w+(?:\:\:\w+)*\s*(?:\(.+\)\s*)*{*'
 DEFINE_REGEX = r'define \w+(?:\:\:\w+)*\s*(?:\(.+\)\s*)*{*'
-#DEFINE_REGEX = r'define\W+.+\W*\(|define\W+.+\{'
 FILE_REGEX = r'file\W*\{\W*\'.+\'\W*:|file\W*\{\W*\".+\"\W*:|file\W*{\W*\$.+\W*:'
 PACKAGE_REGEX = r'package\W*\{\W*\'.+\'\W*:|package\W*\{\W*\".+\"\W*:|package\W*{\W*\$.+\W*:'
 SERVICE_REGEX = r'service\W*\{\W*\'.+\'\W*:|service\W*\{\W*\".+\"\W*:|service\W*{\W*\$.+\W*:'
